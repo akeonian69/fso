efdf20c7791a305c489ea61e646412a7a495f7ba
Amit <ak@gmail.com>
2024-02-11 20:44:40 +0900

    uibuild

---
+++ part3/persons/README.md
@@ -0,0 +1,7 @@
+Link to the online application:
+https://b35f93d8-f9d4-47d8-a8a6-3ed6b374b7f8-00-2nucghodycdsb.kirk.replit.dev
+
+GET:/api/persons
+GET:/api/persons/:id
+POST:/api/persons
+DELETE:/api/persons/:id
--- part3/persons/index.js
+++ part3/persons/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => {
@@ -8,6 +9,8 @@ morgan.token('body', (req, res) => {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -82,7 +85,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
