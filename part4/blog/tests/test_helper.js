746f0da20a3c8c1c0b16eb5be6716abbfe3d05a2
Amit <ak@gmail.com>
2024-02-16 22:32:08 +0900

    exercise 4.16

--- part4/blog/tests/test_helper.js
+++ part4/blog/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -39,11 +40,29 @@ const initialBlogs = [
   }  
 ] 
 
+const initialUsers = [
+  {
+    username: "hellas",
+    name: "Arto Hellas",
+    password: "password1"
+  },
+  {
+    username: "mluukkai",
+    name: "Matti Luukkainen",
+    password: "password2"
+  }
+]
+
 const blogsInDb = async () => {
     const blogs = await Blog.find({})
-    return blogs.map(note => note.toJSON())
+    return blogs.map(blog => blog.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
 }
 
 module.exports = {
-    initialBlogs, blogsInDb
+    initialBlogs, blogsInDb, initialUsers, usersInDb
 }
